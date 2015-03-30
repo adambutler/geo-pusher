eb696eda9f5df2bc79513814df10c4a283c2300d
Adam Butler <adam@lab.io>
2015-03-30 22:25:39 +0100

    Add "Adding Google Maps" section

--- gulp/build.js
+++ gulp/build.js
@@ -13,11 +13,11 @@ gulp.task('partials', ['markups'], function () {
     paths.src + '/{app,components}/**/*.html',
     paths.tmp + '/{app,components}/**/*.html'
   ])
-    .pipe($.minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true
-    }))
+    // .pipe($.minifyHtml({
+    //   empty: true,
+    //   spare: true,
+    //   quotes: true
+    // }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
       module: 'geoPusher'
     }))
@@ -52,11 +52,11 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe($.useref())
     .pipe($.revReplace())
     .pipe(htmlFilter)
-    .pipe($.minifyHtml({
-      empty: true,
-      spare: true,
-      quotes: true
-    }))
+    // .pipe($.minifyHtml({
+    //   empty: true,
+    //   spare: true,
+    //   quotes: true
+    // }))
     .pipe(htmlFilter.restore())
     .pipe(gulp.dest(paths.dist + '/'))
     .pipe($.size({ title: paths.dist + '/', showFiles: true }));
--- src/app/tutorial/partials/01-download.sh
+++ src/app/tutorial/partials/01-download.sh
@@ -1,1 +1,2 @@
+# Note: not setup yet
 git clone -b starter git@github.com:adambutler/geo-pusher.git
